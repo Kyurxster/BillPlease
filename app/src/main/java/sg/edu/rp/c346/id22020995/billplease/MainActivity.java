fcd1df010095193a456f5274a2aa515d702ef36f
Shannon <22020995@myrp.edu.sg>
2023-05-04 16:54:24 +0800

    Finished bill calculations and reset function

--- app/src/main/java/sg/edu/rp/c346/id22020995/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020995/billplease/MainActivity.java
@@ -63,12 +63,86 @@ public class MainActivity extends AppCompatActivity {
                 // Take bill input and display total bill
                 String billInput = etBillInput.getText().toString();
                 int Bill = Integer.valueOf(billInput);
-                tvTotalBill.setText("Total Bill: "+Bill);
-                // Take pax input and display split bill
+                boolean SVSisChecked = tbSVS.isChecked();
+                boolean GSTisChecked = tbGST.isChecked();
+                String discountInput = etDiscountInput.getText().toString();
+                int Discount = Integer.valueOf(discountInput);
                 String paxInput = etPaxInput.getText().toString();
                 int Pax = Integer.valueOf(paxInput);
-                int costPerPax = Bill/Pax;
-                tvSplitCost.setText(String.format("Each Pays: %.2f",costPerPax));
+                int paymentRadioId =rgPayment.getCheckedRadioButtonId();
+
+                // Calculate bill with discount
+                double DiscountedBill = Bill*((100-Discount)/100.0);
+
+
+                if (SVSisChecked){
+                    // If SVS is checked, multiply bill by 1.1 for billWithSVS
+                    double bill = DiscountedBill*(1.1);
+
+                    if (GSTisChecked) {
+                        // If GST is checked, multiply bill by 1.08
+                        double totalBill = bill * (1.08);
+                        double costPerPax = totalBill/Pax;
+                        tvTotalBill.setText(String.format("Total Bill: $%.2f",totalBill));
+
+                        if (paymentRadioId == R.id.radioButtonCash){
+                            // If Cash payment selected, include "in cash"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in cash",costPerPax));
+                        } else {
+                            // If Cash payment selected, include "via paynow to 912345678"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in via PayNow to 912345678",costPerPax));
+                        }
+
+                    } else {
+                        // If GST is not checked, no need add GST
+                        double totalBill = bill;
+                        double costPerPax = totalBill / Pax;
+                        tvTotalBill.setText(String.format("Total Bill: %.2f", totalBill));
+
+                        if (paymentRadioId == R.id.radioButtonCash) {
+                            // If Cash payment selected, include "in cash"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in cash", costPerPax));
+                        } else {
+                            // If Cash payment selected, include "via paynow to 912345678"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in via PayNow to 912345678", costPerPax));
+                        }
+                    }
+
+                } else {
+                    //If SVS is not checked, no need add SVS
+                    if (GSTisChecked) {
+                        // If GST is checked, multiply bill by 1.08
+                        double totalBill = DiscountedBill * (1.08);
+                        double costPerPax = totalBill/Pax;
+                        tvTotalBill.setText(String.format("Total Bill: $%.2f",totalBill));
+
+                        if (paymentRadioId == R.id.radioButtonCash){
+                            // If Cash payment selected, include "in cash"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in cash",costPerPax));
+                        } else {
+                            // If Cash payment selected, include "via paynow to 912345678"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in via PayNow to 912345678",costPerPax));
+                        }
+
+                    } else {
+                        // If GST is not checked, no need add GST
+                        double totalBill = DiscountedBill;
+                        double costPerPax = totalBill/Pax;
+                        tvTotalBill.setText(String.format("Total Bill: %.2f",totalBill));
+
+                        if (paymentRadioId == R.id.radioButtonCash){
+                            // If Cash payment selected, include "in cash"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in cash",costPerPax));
+                        } else {
+                            // If Cash payment selected, include "via paynow to 912345678"
+                            tvSplitCost.setText(String.format("Each Pays: $%.2f in via PayNow to 912345678",costPerPax));
+                        }
+                }
+
+                }
+                // Take pax input and display split bill
+
+
             }
         });
 
@@ -76,14 +150,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Set bill, pax and discount input to 0
-                etBillInput.setText(0);
-                etPaxInput.setText(0);
-                etDiscountInput.setText(0);
+                etBillInput.setText("");
+                etPaxInput.setText("");
+                etDiscountInput.setText("");
                 // Set toggle buttons back to default
-                tbGST.setChecked(true);
-                tbSVS.setChecked(true);
-                // Reset payment radio button
-                rgPayment.setActivated(false);
+                tbGST.setChecked(false);
+                tbSVS.setChecked(false);
+                // Reset payment radio button to cash default
+                rgPayment.check(R.id.radioButtonCash);
+                // Reset total bill and split cost displays
+                tvTotalBill.setText("Total Bill:");
+                tvSplitCost.setText("Each Pays:");
             }
         });
     }
